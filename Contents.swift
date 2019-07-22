f6391d144bb47172443a63a26a921b5c4930ca43
Apple <apple@Apples-MacBook-Pro-73.local>
2019-07-22 12:05:25 -0700

    new git file

---
+++ Contents.swift
@@ -0,0 +1,87 @@
+import Cocoa
+/*
+var str = "Hello, playground"// hi
+// string integer double interpolation variable constant keyword
+// string a cord that holds an output has quotes // var
+//interger whole number
+// floats and double double are more accurate and faster floats slower
+// variables- " 111 lane" hold information adress holds "
+var address = "1313 mockingbird lane"
+// variables reasign by no var address
+address = "254 ocean drive" // create or declare
+// key word color is hot pink
+// let constant
+let name = "charmen"
+var string = "5"
+var empty:Double // data types are alwasy capital//
+print("hello charmen")
+var age = "15"
+var printMe = "hello world"
+print (printMe)
+var new = "charmen is cool"
+var charmen = "yeah what she said"
+var firstname = "jane"
+print (5+5)
+print ("hi this charmen \(14+1)")
+print (new)
+
+true
+false
+
+var temp = 88
+if temp > 88 {
+     print ("then wear shorts")
+}
+
+if temp < 88 {
+   print("then wear pants")
+    
+}
+
+
+
+if temp < 88 {
+    print ("wear pants")
+} else {
+    print ("then wear shorts")
+}
+if (temp == 87){
+ print ("woohoo")
+*/
+var favoriteFood = "pizza"
+    if favoriteFood == "pizza" {
+        print("good choice")
+    }
+    else if favoriteFood == "chiptole" {
+        print("bad choice, pizza is better")
+    }
+        else if favoriteFood == "starbucks" {
+        print("eh choice")
+}
+
+    else if favoriteFood == "ice cream" {
+        print("good choice but pizza is better")
+}
+    else {
+        print("you are weird")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
